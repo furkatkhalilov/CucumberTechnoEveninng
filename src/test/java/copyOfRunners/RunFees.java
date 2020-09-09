ee3b1dc070f69bb51c4d40722a20f4dfe5043677
daulet2030 <daulet2030@gmail.com>
2020-09-09 06:43:58 +0600

    adding separate runners for fee and nationalities

---
+++ src/test/java/copyOfRunners/RunFees.java
@@ -0,0 +1,32 @@
+package copyOfRunners;
+
+import com.cucumber.listener.Reporter;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterClass;
+
+import java.io.File;
+
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-Html-Report",
+                "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport/ExtentReportEnd2End.html" // to generate a extend report we need this plugin
+        },
+        features = {"src/test/java/FeatureFiles/_05_Natianalities.feature"},
+        glue = {"StepDefinition"},
+        dryRun = false
+)
+
+public class RunFees extends AbstractTestNGCucumberTests {
+    @AfterClass
+    public static void afterClass(){
+
+//                Location of the xml file
+        Reporter.loadXMLConfig(new File("src/test/java/FeatureFiles/extentReport.xml"));
+        Reporter.setSystemInfo("User Name" , "Ali Koklu");
+        Reporter.setSystemInfo("Application Name", "Basqar");
+        Reporter.setSystemInfo("Operating System Type" , System.getProperty("os.name"));
+        Reporter.setSystemInfo("Environment", "QA");
+        Reporter.setTestRunnerOutput("Test execution Cucumber report");
+    }
+}
